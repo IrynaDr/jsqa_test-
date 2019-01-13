c98dfd84895833e8d8cb50efe1f3cdb422586c0d
dranovska <dranovskaa@gmail.com>
2019-01-13 23:53:03 +0200

    home work

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,11 @@
 
 
-require('./course/index');
+const course = require('./course/index');
 
 console.log(startDate);
+
+
+
+// In app.js file, in config loading callback, call the course.setStartDate method
+// and pass to it the value loaded from the config's startDate property.
+// Tip: you may need to use the JSON.parse method to get the value from loaded data.
--- src/app/course/index.js
+++ src/app/course/index.js
@@ -20,3 +20,39 @@
             return startDate;
         }
 })();
+
+// In the course module, export a new method, setStartDate(value).
+// This method should set the startDate property to the value argument.
+
+(function () {
+    //Built-in NodeJS Modules
+    const fs = require('fs');
+    const path = require('path');
+    require('../app.config');
+
+    let _config = '[Empty config]';
+    const jsonPath = path.join(__dirname, '..', 'data', 'app.config.json' );
+
+    function _parseData(err, data) {
+        if (err){
+            throw err;
+        }
+        _config = data;
+        console.log('Loaded config: ', _config);
+    }
+
+    function _loadConfig() {
+        fs.readFile(jsonPath, 'utf8', '_parseData')
+    }
+
+//Export Module API
+    exports.setStartDate = () => {
+        return _config;
+    }
+    exports.loadConfig = () => {
+        return _loadConfig;
+    }
+
+})();
+
+
